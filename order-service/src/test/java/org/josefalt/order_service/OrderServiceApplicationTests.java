3709b0d721dfabe55a4d65c23792daec3ff3b55f
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2024-10-18 19:14:09 -0500

    created service to handle car orders

---
+++ order-service/src/test/java/org/josefalt/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.josefalt.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
