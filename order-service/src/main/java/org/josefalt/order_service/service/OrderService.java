59c131f253f460c80c4e627a5037e94a545a3e23
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2024-11-22 15:12:56 -0600

    made service transactional

--- order-service/src/main/java/org/josefalt/order_service/service/OrderService.java
+++ order-service/src/main/java/org/josefalt/order_service/service/OrderService.java
@@ -8,6 +8,7 @@ import org.josefalt.order_service.model.Order;
 import org.josefalt.order_service.model.OrderItem;
 import org.josefalt.order_service.repository.OrderRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@Transactional
 public class OrderService {
 
 	/**
